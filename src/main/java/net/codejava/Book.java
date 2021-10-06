c203beedd01bd38a4b051743c2e551c4d30657b0
13934447 <62012692+13934447@users.noreply.github.com>
2021-10-06 20:59:53 +1100

    update1

--- src/main/java/net/codejava/Book.java
+++ src/main/java/net/codejava/Book.java
@@ -1,5 +1,58 @@
 package net.codejava;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "books")
 public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, length = 64)
+	private String title;
+	@Column(nullable = false, length = 64)
+	private String author;
+	@Column(nullable = false, length = 64)
+	private String genre;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
 
 }
